f3215e88a6471157439df6014096919038de19b2
Gulshan Sharma <100503295+Gulshan-AIMLOps@users.noreply.github.com>
2023-09-05 10:41:10 +0530

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,7 +9,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3.8 python3.8-venv
 
 echo "Making Python 3.8 the default..."
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 2
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 2
 
 echo "Checking the Python version..."
 python3 --version
